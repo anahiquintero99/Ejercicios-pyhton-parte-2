1e0361fc77b9b041ad2ce1c392e9bd5381b421f9
Anahi <anitaquintero99@gmail.com>
2020-07-28 22:06:12 -0500

    Ejercicio de python con condicionales

---
+++ conditional.py
@@ -0,0 +1,14 @@
+# age = int(input('Escribe tu edad: '))
+# if age > 17 :
+    # print('Eres mayor edad')
+# else:
+    # print('Eres menor de edad')
+
+numero = int(input('Escribe un numero: '))
+
+if numero > 5:
+    print('Es mayor a 5')
+elif numero == 5:
+    print('Es igual a 5')
+else:
+    print('Es menor a 5')
